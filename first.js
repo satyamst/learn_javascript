158ea37fdc58cd85878e30f94c55b45371df1709
satymst <satyamtiwari1091@gmail.com>
2023-11-28 22:57:32 +0530

    modification

---
+++ first.js
@@ -0,0 +1,95 @@
+// const product = {
+//     product_name:'Orange',
+//     product_id:1,
+//     product_price:100,
+//     product_categoriya:'frute',
+
+// };
+//for loop for js script
+// let count = 20;
+// for(let count =1; count<=5;count++){
+//     console.log("count");//5 execute five times
+// }
+alert("hello friend");
+
+/*
+// calculate sum of 1 to 5
+let sum = 0;
+for(let i =1; i<=5;i++) {
+    sum= sum+i;
+}
+console.log("sum",sum);
+console.log("loop is ended");
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let sum = 5;
+// // let n = 100;
+// for (let i=1; i<= 5; i++) {
+//     sum = sum + i; // sum = 15 
+// }
+// console.log("sum",sum);
+// console.log("loop has be ended");
+//print 1 to 5 in for loops 
+/*for(let i=1;i<=5;i++) {
+    console.log("i =",i);
+}
+let i=0;
+console.log(i);
+*/
+//infinity loop
+//infinite loop :A Loop  
+
+// while(condition) {
+    //do some 
+//}
+/*let i=1;
+while(i<=5) {
+    i++;
+    console.log("satyam tiwari",i);
+    // i++;
+}
+
+*/
+/*
+do {
+   // do some work 
+}
+while (condition);
+*/
+/*let i=20;
+do {
+    console.log("satyam coder=",i);
+    i++;
+} while (i<=10);
+
+for-of loop & for-in
+
+for(let val of strVar) {
+    // do some work
+}
+*/
+/*let str = "Satyamcoder";
+
+for(let i of str) {
+    console.log("i=", i);
+}
+
+*/
+
+
+
+
+
+// console.log(product);
